5e4f156fbde5040ef4a83df3a17d1af2ae646fa8
kennymasTech <141999681+kennymasTech@users.noreply.github.com>
2023-10-19 17:54:39 +0100

    lesson-14-RefreshToken

---
+++ lesson-14-RefreshToken/controllers/authController.js
@@ -0,0 +1,54 @@
+  const bcrypt = require('bcrypt');
+  const jwt = require('jsonwebtoken');
+  const User = require('../model/User')
+  
+  const handleLogin = async (req, res) => {
+    const cookies = req.cookies
+    console.log(`cookie available at login: ${JSON.stringify(cookies)}`);
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
+
+    const foundUser = await User.findOne({username: user}).exec();
+    if (!foundUser) return res.sendStatus(401); //Unauthorized 
+
+    // evaluate password 
+    const match = await bcrypt.compare(pwd, foundUser.password);
+    if (match) {
+      const roles = Object.values(foundUser.roles)
+        // create JWTs
+        const accessToken = jwt.sign(
+            { 
+              "UserInfo" : {
+              "username": foundUser.username,
+              "roles" : roles
+              }
+            } ,
+            process.env.ACCESS_TOKEN_SECRET,
+            { expiresIn: '60s' }
+        );
+        const newRefreshToken = jwt.sign(
+            { "username": foundUser.username },
+            process.env.REFRESH_TOKEN_SECRET,
+            { expiresIn: '1d' }
+        );
+
+          const newRefreshTokenArray = 
+              !cookies?.jwt
+                  ? foundUser.refreshToken
+                  : foundUser.refreshToken.filter(rt => rt !== cookies.jwt)
+
+              if (cookies) res.clearCookie('jwt', { httpOnly: true, sameSite: 'None'});
+
+          foundUser.refreshToken = [...newRefreshTokenArray, newRefreshToken]
+          const result = await foundUser.save();
+          console.log(result);
+
+        console.log(result);
+        res.cookie('jwt', newRefreshToken, { httpOnly: true, sameSite: 'None', maxAge: 24 * 60 * 60 * 1000 });
+        res.json({ accessToken });   // secure: true, 
+    } else {
+        res.sendStatus(401);
+    }
+  }
+  
+  module.exports = { handleLogin };
